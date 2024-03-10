c8660ba8cfb5300927d19fdb65fb5c02b92226e6
Vivek <vivek66anand66@gmail.com>
2024-03-10 17:27:45 +0530

    all code

---
+++ Episode2/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+
+const parent = React.createElement("h1", {}, " React is on 🚀")
+
+const root = ReactDom.createRoot(document.getElementById(root));
+root.render(parent);
